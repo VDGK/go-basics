2cc209235115b827651c327f30f9d5bcdd89612a
mikesamoylove <mikesamoylove@yandex.ru>
2025-10-20 20:40:09 +0300

    add go-demo-4 project

--- go-demo-4/main.go
+++ go-demo-4/main.go
@@ -1,14 +1,67 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"math/rand/v2"
+	"net/url"
+	"os"
+)
+
+type account struct {
+	login    string
+	password string
+	url      string
+}
+
+func (acc *account) outputPassword() {
+	fmt.Println(acc.login, acc.password, acc.url)
+}
+
+func (acc *account) generatePassword(length int) {
+	res := make([]rune, length)
+	for i := range res {
+		res[i] = letterRunes[rand.IntN(len(letterRunes))]
+	}
+	acc.password = string(res)
+}
+
+func newAccount(login, password, urlString string) (*account, error) {
+	if login == "" {
+		return nil, errors.New("login required")
+	}
+	_, err := url.ParseRequestURI(urlString)
+	if err != nil {
+		return nil, fmt.Errorf("неверный URL-адрес %q: %w", urlString, err)
+	}
+	newAcc := &account{
+		login:    login,
+		url:      urlString,
+		password: password,
+	}
+	if password == "" {
+		newAcc.generatePassword(20)
+	}
+	return newAcc, nil
+}
+
+var letterRunes = []rune("qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890-*!@#$%^&*()_+-=")
 
 func main() {
-	arr := [4]int{1, 2, 3, 4}
-	reverse(&arr)
-	fmt.Println(arr)
-}
-func reverse(arr *[4]int) {
-	for index, value := range *arr {
-		(*arr)[len(arr)-index-1] = value
+	login := promptData("Input Login")
+	password := promptData("Input Password")
+	urlString := promptData("Input URL")
+	myAccount, err := newAccount(login, password, urlString)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
+		os.Exit(1)
 	}
+	myAccount.outputPassword()
+	fmt.Println(myAccount.login, myAccount.password, myAccount.url)
+}
+func promptData(prompt string) string {
+	fmt.Println(prompt + ":")
+	var result string
+	_, _ = fmt.Scanln(&result)
+	return result
 }
